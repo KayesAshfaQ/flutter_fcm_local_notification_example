7952e16c33f960b29f01726b50e6f2775cf5aa42
kayesAshfaq <asfak7074@gmail.com>
2024-09-30 13:30:21 +0600

    feat: initialize Firebase messaging service in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'api/firebase_message_api.dart';
 import 'firebase_options.dart';
 import 'my_app.dart';
 
-void main() {
+void main() async {
   // ensure flutter binding is initialized
   WidgetsFlutterBinding.ensureInitialized();
 
   // initialize firebase app
-  Firebase.initializeApp(
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // initialize firebase messaging service
+  await FirebaseMessageApi().initialize();
 
   // run the app
   runApp(const MyApp());
